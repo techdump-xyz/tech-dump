fc1d650952bd7796aa8ce3876ba63addd564e98c
Ondřej Štěch <ondrastech82@gmail.com>
2023-05-02 11:32:23 +0200

    feat: added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   markdown: {
     shikiConfig: {
-      theme: "monokai",
+      theme: "monokai"
     }
   },
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()]
 });
