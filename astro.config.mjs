92f07511f6f73da451420dbeab5b17aa5258382f
Ondřej Štěch <ondrastech82@gmail.com>
2023-10-13 15:02:43 +0200

    chore: removed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import { BASE } from './src/utils/baseUrl';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ondrastech82.github.io",
-  base: BASE,
   markdown: {
     shikiConfig: {
       theme: "monokai"
